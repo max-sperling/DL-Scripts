351b49aca50ed730281384adfc30df442060e82e
Max Sperling <sperling1991@gmail.com>
2024-07-29 15:47:02 +0200

    Download retry happens now right away

--- download_pattern.py
+++ download_pattern.py
@@ -19,18 +19,17 @@ def search_items(text, pattern):
 
 def download_items(items):
     for item in items:
-        download_utils.print_message(f"Download File: {item}")
         item_name = download_utils.get_without_webpath(item)
 
-        try:
-            download_utils.download_file(item, item_name)
-        except Exception as e:
-            download_utils.print_message(f"File: {item}, Error: {str(e)}")
+        if download_utils.download_file(item, item_name):
+            download_utils.print_message(f"Download successful: {item}")
+        else:
+            download_utils.print_message(f"Download failed: {item}")
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--url', type=str, required=True)
-    parser.add_argument('-p', '--pattern', type=str, required=True)
+    parser.add_argument('-u', '--url', type = str, required = True)
+    parser.add_argument('-p', '--pattern', type = str, required = True)
 
     args = parser.parse_args()
     download_pattern(args.url, args.pattern)
--- download_utils.py
+++ download_utils.py
@@ -3,12 +3,21 @@ import os
 import requests
 import urllib
 
-def download_file(url, filename):
-    if not os.path.isfile(filename):
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
-        with open(filename, 'wb') as f:
-            f.write(response.content)
+def download_file(url, filename, retries = 3):
+    if os.path.isfile(filename):
+        return True
+
+    while retries > 0:
+        try:
+            response = requests.get(url, timeout = 10)
+            response.raise_for_status()
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+            return True
+        except Exception as e:
+            retries -= 1
+
+    return False
 
 def get_without_webargs(link):
     return link.split('?')[0]
@@ -16,18 +25,14 @@ def get_without_webargs(link):
 def get_without_webpath(link):
     return link.split('/')[-1]
 
-def call_until_successful(max_iterations, func, *args):
-    for _ in range(max_iterations):
-        if func(*args):
-            return True
-    return False
-
 def print_message(message):
-    print(f"{datetime.datetime.now()} {message}")
+    print(f"{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {message}")
 
 def remove_duplicates(items):
     unique_items = []
+
     for item in items:
         if item not in unique_items:
             unique_items.append(item)
+
     return unique_items
