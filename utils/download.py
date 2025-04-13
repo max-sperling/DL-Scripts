bbd530324e231be9107f3518f2d362c5f40cb772
Max Sperling <sperling1991@gmail.com>
2025-04-13 19:33:43 +0200

    Delete media file if download fails even after retries

--- utils/download.py
+++ utils/download.py
@@ -15,4 +15,6 @@ def download_file(url, file, headers = {}, attempts = 3):
         except Exception as e:
             attempts -= 1
             if attempts == 0:
+                if os.path.isfile(file):
+                    os.remove(file)
                 raise
